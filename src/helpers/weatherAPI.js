dc2fb0d7eda72480973681c577bbbf52f4fdb47e
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-08-09 23:40:11 -0300

    Implementada função searchCities

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,17 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+
+export const searchCities = async (TERMO_DE_BUSCA) => {
+  const URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${TERMO_DE_BUSCA}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  if (data.length < 1) {
+    window.alert('Nenhuma cidade encontrada')
+  } else {
+    return data
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+  //   seu código aqui
 };
